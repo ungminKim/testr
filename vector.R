bd36eeb4f3b813dfa1b99ad0ec3bb6284e0356f8
ungminKim <david283@naver.com>
2020-04-01 14:29:57 +0900

    week3-2 vertor

---
+++ vector.R
@@ -0,0 +1,62 @@
+2^3
+log(10)+5
+sqrt(25)
+max(5,3,2)
+a <- 125
+print(a)
+a <- 10
+b <- 20
+c <- a + b
+c
+c
+Sys.setlocale("LC_ALL")
+a <- 한글
+a <- "한글"
+Sys.setlocale("LC_ALL", "English")
+a <- "한글"
+a
+a <- "한글이"
+ㅁㄴ
+x <- c(1:10)
+x <- 1:10
+x
+y <- c(x, 3:1)
+y
+1/0
+1/inf
+1/Inf
+z <- 0:10:2
+x <- vector("numeric", length=10)
+x
+y <- c(1.7, "a")
+y
+y <- c(TRUE, 2)
+y
+y <- c("a", TRUE)
+y
+x <- c("a", "b", "c")
+as.numeric(x)
+year <- c(1985, 1999, 2010, 2002)
+names(year) <- c("The Breakfast Club", "American Beauty", "Black Swan", "Chicago")
+year
+year["Black Swan"]
+year[1]
+year[2]
+year[1:2]
+year[-1]
+a <- c(1, 2, 3)
+a
+b <- c(TRUE, TRUE, FALSE)
+a[b]
+a
+v3 <- seq(1, 101, 3)
+v3
+v4 <- seq(0.1, 1.0, 0.2)
+v4
+v5 <- rep(1, times=5)
+v5
+help("sort")
+
+b <- 20
+c <- a + b
+c
